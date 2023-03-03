29dce00b1eea8e07e195e5a046e40e6918099910
子锟 雷 <2667272459@qq.com>
2023-03-03 23:11:32 +0800

    第一次作业更新

---
+++ assignment_1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ArrayLoop
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Please input the first num: ");
+        string fn = Console.ReadLine();
+        int f = Int32.Parse(fn);
+        Console.Write("input the operator: ");
+        string op = Console.ReadLine();
+        Console.Write("Please input the second num: ");
+        string sn = Console.ReadLine();
+        int s = Int32.Parse(sn);
+        int result = 0;
+
+        switch (op)
+        {
+            case "+":
+                result = f + s;
+                break;
+            case "-":
+                result = f - s;
+                break;
+            case "*":
+                result = f * s;
+                break;
+            case "/":
+                result = f / s;
+                break;
+            default:
+                Console.WriteLine("False");
+                break;
+        }
+        Console.WriteLine("The result is " + result);
+    }
+}
---
+++ assignment_1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        //结果
+        int res = 0;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //button1 : +
+        //button2 : -
+        //button3 : *
+        //button4 : /
+        //button5 : calculate
+        private void button1_Click(object sender, EventArgs e)
+        {
+            res = Int32.Parse(textBox1.Text) + Int32.Parse(textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            res = Int32.Parse(textBox1.Text) - Int32.Parse(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            res = Int32.Parse(textBox1.Text) * Int32.Parse(textBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            res = Int32.Parse(textBox1.Text) / Int32.Parse(textBox2.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = res.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
